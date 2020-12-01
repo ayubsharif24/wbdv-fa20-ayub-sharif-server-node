34d07be353f9a902c46e19f89b2ea31a085e403e
ayubsharif <@ccs.neu.edu>
2020-12-01 15:55:19 -0500

    committing updated server.js

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+var bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 
+
+
 const mongoose = require('mongoose')
-mongoose.connect()
+mongoose.connect('mongodb+srv://dbUser:dbUserPassword@cluster0.c1s1o.mongodb.net/whiteboard?retryWrites=true&w=majority',
+                 { useNewUrlParser: true, useUnifiedTopology: true})
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -13,10 +20,14 @@ app.use(function (req, res, next) {
     next();
 });
 
+
+
 const quizzesController = require('./controllers/quizzes.controller.server')
 const questionsController = require('./controllers/questions.controller.server')
+const quizAttemptController  = require('./controllers/quiz-attemps-controller.server')
 
 quizzesController(app);
 questionsController(app);
+quizAttemptController(app);
 
 app.listen(process.env.PORT)
